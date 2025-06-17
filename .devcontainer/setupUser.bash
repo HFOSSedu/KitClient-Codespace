d63c0ad9c8eec798b8391dd8a4f1285e613efd32
braughtg <braught@dickinson.edu>
2025-06-17 14:57:36 -0400

    try a sleep

--- .devcontainer/setupUser.bash
+++ .devcontainer/setupUser.bash
@@ -20,6 +20,8 @@ if [ ! -d /workspaces/$USER ]; then
    && echo "source /usr/share/bash-completion/completions/git" >> "/home/$USER/.bashrc" 
 fi
 
+sleep 3
+
 # If the user's home directory (or the link to it) is the open folder, then
 # reopen VSCode without any folder open.
 if [ "${CODESPACE_VSCODE_FOLDER}" == "/home/vscode" ] || [ "${CODESPACE_VSCODE_FOLDER}" == "/workspaces/vscode" ]; then
